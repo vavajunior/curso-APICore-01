9c845f4658ede0849d36066bea666f88abf01b16
Osvaldo Junior <vavajunior@gmail.com>
2022-08-04 18:42:19 -0300

    Add project files.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using curso_APICore_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace curso_APICore_01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public static List<Filme> listaFilmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            listaFilmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace curso_APICore_01.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage ="O campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 300, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 300 minutos")]
+        public int Duracao { get; set; }
+    }
+}
